d994a40c79d653e67bc66e3b42d3d5a96aa17142
Ferve <ferve91@gmail.com>
2022-08-29 11:24:37 -0300

    Se agrego el iframe para que se pueda localizar y ver el mapa de la ciudad, luego todos los parametros para ver el tiempo y la temp de la localidad buscada

--- widget-weather/src/components/weatherApp.js
+++ widget-weather/src/components/weatherApp.js
@@ -1,12 +1,39 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import WeatherForm from './weatherForm';
+import WeatherMainInfo from './weatherMainInfo';
+import styles from './weatherApp.module.css';
 
 export default function WeatherApp(){
 
     const [weather, setWeather] = useState(null)
-    return <div>
-        <form>
-            
-        </form>
-        <div>Info</div>
+
+    useEffect(()=>{
+        loadInfo();
+    },[]);
+
+    useEffect(()=>{
+        document.title = `Weather | ${weather?.location.name ?? ""}`;
+    },[weather]);
+
+    async function loadInfo(city = 'london'){
+        try{
+            const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`);
+            const json = await request.json();
+            setWeather(json);
+            console.log(json);
+        } catch {
+
+        }
+    }
+
+    function handleChangeCity(city){
+        setWeather(null);
+        loadInfo(city);
+    }
+
+    return <div className={styles.weatherContainer}>
+        <WeatherForm onChangeCity={handleChangeCity}/>
+        {/* {weather?.current.temp_c} el ? significa que es una propiedad opcional, caso que no exista no tira error */}
+        <WeatherMainInfo weather={weather} />
     </div>
 }
